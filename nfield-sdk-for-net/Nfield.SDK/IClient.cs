9db464cadf555683a180554cdf06c8e4be98489c
bjorncoltof <bjorn.coltof@gmail.com>
2013-03-12 17:01:01 +0100

    Added client and client factory stubs

---
+++ nfield-sdk-for-net/Nfield.SDK/IClient.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Nfield
+{
+    public interface IClient : IDisposable
+    {
+    }
+}
---
+++ nfield-sdk-for-net/Nfield.SDK/IClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nfield
+{
+    public interface IClientFactory
+    {
+        IClient Create(
+            string domainName,
+            string userName,
+            string password);
+    }
+}
